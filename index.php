8d9c13b84df4cf2818b758fdb0a5bb94b7cd5e9d
Riley Bowling <rcbowling03@gmail.com>
2024-04-01 19:30:53 -0600

    created files / added comments for files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!--
+Hello everyone!
+
+Just so everyone is on the same page, the file structure should be as follows:
+local_html
+    group32
+        Byte-By-Bite
+            index.php
+            other files yadda yadda 
+
+the url for this project is: https://cs.colostate.edu:4444/~EID/group32/Byte-By-Bite/index.php
+
+THIS FILE WILL CONTAIN CODE FOR THE INDEX/WELCOME PAGE
+-->
