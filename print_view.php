940590abbf170f66664acfd7c4034c6a1007ccca
Elizabeth Kilby <elizkilb@columbia.cs.colostate.edu>
2024-04-08 01:14:46 -0600

    remove grayscale- wasn't working properly

--- print_view.php
+++ print_view.php
@@ -1,9 +1,6 @@
-<link rel="stylesheet" type="text/css" href="./style.css">
 
+<?php include 'tables.php'; ?>
 
-<div class="grey">
-    <?php include 'tables.php'; ?>
-</div>
 
 <script>
     window.print();
