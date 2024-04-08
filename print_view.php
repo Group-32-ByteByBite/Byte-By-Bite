5fa40af9cd9efc435562c01159ac932f98aa890a
Elizabeth Kilby <elizkilb@columbia.cs.colostate.edu>
2024-04-08 01:02:14 -0600

    added grayscale

--- print_view.php
+++ print_view.php
@@ -1,4 +1,10 @@
-<?php include 'tables.php'; ?>
+<link rel="stylesheet" type="text/css" href="./style.css">
+
+
+<div class="grey">
+    <?php include 'tables.php'; ?>
+</div>
+
 <script>
     window.print();
     </script>
