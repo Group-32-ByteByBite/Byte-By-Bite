499fe1da041782ae08d1dbb5ddb1d7ae48294a66
Elizabeth Kilby <elizkilb@columbia.cs.colostate.edu>
2024-04-08 01:01:25 -0600

    added css

--- parameter_selection.php
+++ parameter_selection.php
@@ -1,15 +1,17 @@
 <!-- this file will contain code for the parameter selection page -->
-
+<link rel="stylesheet" type="text/css" href="./style.css">
 <?php
 
 ?>
 
 <html>
     <body>
-       
-        <form name="colorForm" action='tables.php' method="GET" >
-            Number of rows and columns: <input type="text" name="numColsRows" />
-            Number of colors: <input type="text" name="colors" />
-            <input type="submit" value="Generate Tables" />     
+        <div class="selection"></div>
+            <h2> Color Coordinates Generator</h2>
+            <form name="colorForm" action='tables.php' method="GET" >
+                <div class= "rowColumn"> Number of rows and columns: <input type="text" name="numColsRows" /><div>
+                <div class= "rowColumn">Number of colors: <input type="text" name="colors" /></div>
+                <div class="submitButton"><input type="submit" value="Generate Tables" /></div>    
+        </div>
 </body>
 </html>
